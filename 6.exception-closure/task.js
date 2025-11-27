ee8892550e6ca523230a5e3d8f126cc2de0d3003
Dmitriy Karpukhin <kid-87@inbox.ru>
2025-11-27 23:47:21 +0300

    Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿//Задача №1.
+function parseCount(number){
+    let result = Number.parseFloat(number);
+    if (isNaN(result)){
+        throw new Error ('Невалидное значение');
+    }
+    return result;
+}
+
+function validateCount(number){
+    try{
+        return parseCount(number);
+    } catch(error){
+        return error;
+    }
+}
+
+//Задача № 2.
+class Triangle{
+    constructor(a, b, c){
+        if(a + b < c || a + c < b || b + c < a){
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+
+    get area(){
+        const p = (this.a + this.b + this.c) / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return parseFloat(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c){
+    try{
+        return new Triangle(a, b, c);
+    } catch (error){
+        return{
+            get perimeter(){
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area(){
+                return 'Ошибка! Треугольник не существует';
+            }
+        };
+    }
+}
