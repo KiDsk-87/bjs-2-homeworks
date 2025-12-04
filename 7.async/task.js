0362068f9d62deccc33a23ca3283aa345b8d947f
Dmitriy Karpukhin <kid-87@inbox.ru>
2025-12-05 00:15:08 +0300

    Асинхронность

---
+++ 7.async/task.js
@@ -0,0 +1,58 @@
+class AlarmClock{
+    constructor(){
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback){
+        if(!time || !callback){
+            throw new Error ('Отсутствуют обязательные аргументы');
+        }
+        if(time in this.alarmCollection){
+            console.warn('Уже присутствует звонок на это же время');
+        }
+        this.alarmCollection.push({
+            callback,
+            time,
+            canCall: true
+        });
+    }
+
+    removeClock(time){
+        this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
+    }
+
+    getCurrentFormattedTime(){
+        const now = new Date();
+        return now.toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit', hour12: false });
+    }
+
+    start(){
+        if(this.intervalId){
+            return;
+        }
+        this.intervalId = setInterval(() => {
+            const currentTime = this.getCurrentFormattedTime();
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === currentTime && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        }, 1000);
+    }
+
+    stop(){
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls(){
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
